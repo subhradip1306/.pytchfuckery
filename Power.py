0f5ddccb1977350aa0fd8d84f1a71c7ff51b28d4
Subhradip <subhradip6969@gmail.com>
2024-10-01 14:03:08 +0530

    Update Power.py
    it fucking works now. Takes up a lot of computing power however.

--- Power.py
+++ Power.py
@@ -1,29 +1,41 @@
 import math
+import random
 
 n=int(input("What is the cardinality of the set? "))
 setX=set()
 setY=set()
 
 for i in range(n):
-    x=int(input("Please enter an element: "))
+    x=(input("Please enter an element: "))
     
     setX.add(x)
     setY.add(x)
+
 subset=set()
 subset.update(setY)
-print(setX)
-print(subset)
+
 powerset={frozenset(setX), frozenset(set())}
-for k in range(2**n):
-    
-    for m in setX:
-            subset.remove(m)
-            print("subset is", subset)
-            powerset.add(frozenset(subset))
+
+if len(setX)>1:
+
+    for k in range(2**(n+int(n/2))):
+        subLIST=list(subset)
+                
+        m=random.randint(0, len(subset)-1)
+                
+        subLIST.pop(m)
+        subset=set(subLIST)
+                
+        powerset.add(frozenset(subset))
+                
+        if len(subset)==1:
             
-            if len(subset)==1:
-                print(setY)
-                subset.update(setY)
-                print("jhingalala", subset)
-print(powerset)   
+            subset.update(setY)
+    
+    
+powerLIST=list(powerset)
+
+for j in range(len(powerset)):
+    print(powerLIST[j])   
+
 print(len(powerset))
