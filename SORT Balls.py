3133a05d96946e6aec6f35e5e35227dc9c1623f6
Subhradip <subhradip6969@gmail.com>
2024-10-17 22:09:33 +0530

    Added SORT Balls

---
+++ SORT Balls.py
@@ -0,0 +1,37 @@
+#Smallest number of swaps reqd to sort balls
+
+import random
+
+random.seed()
+leng=int(1+10*(random.random()))
+
+print(leng)
+
+INITIAL=[]
+
+for i in range(leng):
+    
+    dig=random.randint(0,1)
+    dig=str(dig)
+    INITIAL.append(dig)
+
+INITIALstr=''.join(INITIAL)
+print(INITIALstr)
+
+checkA=[]
+checkB=[]
+
+for j in range(int(leng/2)):
+    
+    if INITIAL[j]==INITIAL[0]:
+        checkA.append(j)
+ 
+for k in range(leng-int(leng/2)):
+    if INITIAL[leng-k]!=INITIAL[0]:
+        checkB.append(leng-k)
+        
+print(checkA) 
+print(checkB)
+  
+
+    
